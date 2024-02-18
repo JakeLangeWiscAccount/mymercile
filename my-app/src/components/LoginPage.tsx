c5d8a817bbc847c54df8415064c5418cc9788eaf
Jacob lange <jacoblange007@yahoo.com>
2024-02-17 19:23:36 -0600

    last work for day

--- my-app/src/components/LoginPage.tsx
+++ my-app/src/components/LoginPage.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { Box } from "@mui/system";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 
@@ -6,15 +7,59 @@ const LoginPage = () => {
 
   const navigate = useNavigate();
 
-  const handleClick = () => {
-    navigate("/");
-  };
+  
 
   //create user name and password
 
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const profileFormSubmitHandler = async(e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    // Handle form submission logic here (e.g., send data to server)
+    console.log("Submitted:", {
+       username, password
+    });
+
+    navigate("/home");
+
+  };
+
+
   return (
     <div>
-      Login Page
+      <form onSubmit={profileFormSubmitHandler}>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column", // Render elements vertically
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+          }}
+        >
+            Please Login
+          <label>
+            Username:
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </label>
+
+          <label>
+            Password:
+            <input
+              type="text"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+          <button type="submit">Submit</button>
+          </Box>
+          
+          </form>
     </div>
   );
 };
--- my-app/src/components/RegistrationPage.tsx
+++ my-app/src/components/RegistrationPage.tsx
@@ -1,9 +1,83 @@
-import React from "react";
+import { Box } from "@mui/system";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 
 const RegistrationPage = () => {
+
+  const navigate = useNavigate();
+
+  
+
+  //create user name and password
+
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [confirmedPassword, setConfirmedPassword] = useState<string>("");
+  const [registrationFailed, setRegistrationFailed] = useState<boolean>(false);
+
+  const profileFormSubmitHandler = async(e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    // Handle form submission logic here (e.g., send data to server)
+    console.log("Submitted:", {
+       username, password
+    });
+
+    if (confirmedPassword == password){
+      navigate("/home");
+    }else{
+      setRegistrationFailed(true);
+      setUsername("");
+      setPassword("");
+      setConfirmedPassword("");
+    }
+    
+  };
+
+
   return (
     <div>
-      Registration Page
+      <form onSubmit={profileFormSubmitHandler}>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column", // Render elements vertically
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+          }}
+        >
+            Please Login
+          <label>
+            Username:
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </label>
+
+          <label>
+            Password:
+            <input
+              type="text"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+          <label>
+            Confirm Password:
+            <input
+              type="text"
+              value={confirmedPassword}
+              onChange={(e) => setConfirmedPassword(e.target.value)}
+            />
+          </label>
+          <button type="submit">Submit</button>
+          {registrationFailed && <div>Registration Failed! Try Again!</div>}
+          </Box>
+          
+          </form>
     </div>
   );
 };
